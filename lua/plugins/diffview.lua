6f96bef761112c7bc96385e1e2b695ecae026ea0
Florian Sanders <florian.sanders@clever-cloud.com>
2023-12-29 19:01:53 +0100

    feat(typescript lsp + diffview): remove volar, fix diffview keymap

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -5,7 +5,7 @@ return {
     keys = {
       { "<leader>gg", "<cmd>DiffviewOpen<CR>", mode = { "n" }, desc = "Open current status and diff" },
       { "<leader>gf", "<cmd>DiffviewFileHistory<CR>", mode = { "n" }, desc = "Open project history" },
-      { "<leader>gq", "<cmd>DiffviewClose<CR>", mode = { "n", "i", "v" }, desc = "Close Diffview" },
+      { "<leader>gq", "<cmd>DiffviewClose<CR>", mode = { "n" }, desc = "Close Diffview" },
     },
     dependencies = {
       "nvim-lua/plenary.nvim",
