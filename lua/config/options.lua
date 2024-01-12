723507cabd5374aafb5597eb61cc2e1011557628
Florian Sanders <florian.sanders@clever-cloud.com>
2024-01-12 19:01:44 +0100

    feat(neovide): init config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,25 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 -- not sure this is still necessary, TODO: check
-vim.g.root_spec = { "cwd" }
+vim.g.root_spec = { ".git", "cwd" }
 vim.g.markdown_fenced_languages = {
   "ts=typescript",
 }
+if vim.g.neovide then
+  -- Put anything you want to happen only in Neovide here
+  -- vim.o.guifont = {
+  --   normal = { family = "JetBrainsMonoNL Nerd Font", style = "Thin" },
+  --   bold = { family = "JetBrainsMonoNL Nerd Font", style = "SemiBold" },
+  -- --   bold_italic = { family = "JetBrainsMonoNL Nerd Font", style = "SemiBold Italic" },
+  -- }
+  vim.g.neovide_padding_top = 20
+  vim.g.neovide_padding_bottom = 20
+  vim.g.neovide_padding_right = 20
+  vim.g.neovide_padding_left = 20
+
+  vim.g.neovide_cursor_trail_size = 0.1
+  vim.g.neovide_scroll_animation_length = 0.1
+  vim.g.neovide_cursor_vfx_mode = "pixiedust"
+
+  vim.g.neovide_profiler = false
+end
