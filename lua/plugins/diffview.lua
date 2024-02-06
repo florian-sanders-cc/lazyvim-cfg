d0760f6feff9a3cfeba4f2212838b32b48f87711
Florian Sanders <florian.sanders@clever-cloud.com>
2024-02-06 15:39:51 +0100

    feat(diffview): enable better hl & 3 way diff in merge tool

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -7,6 +7,17 @@ return {
       { "<leader>gf", "<cmd>DiffviewFileHistory<CR>", mode = { "n" }, desc = "Open project history" },
       { "<leader>gq", "<cmd>DiffviewClose<CR>", mode = { "n" }, desc = "Close Diffview" },
     },
+    opts = {
+      enhanced_diff_hl = true,
+      view = {
+        merge_tool = {
+          -- Config for conflicted files in diff views during a merge or rebase.
+          layout = "diff3_mixed",
+          disable_diagnostics = true, -- Temporarily disable diagnostics for conflict buffers while in the view.
+          winbar_info = true, -- See ':h diffview-config-view.x.winbar_info'
+        },
+      },
+    },
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
