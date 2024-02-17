4f5527e57bbe4c73a4ed59660b60803fc1f0dc61
Florian Sanders <florian.sanders@clever-cloud.com>
2024-02-17 20:52:37 +0100

    feat(cmp): enable emmet

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,54 +1,12 @@
-if true then
-  return {}
-end
--- I would like to only get emmet suggestions within a lit template but I don't know how to do this right now.
 return {
   "hrsh7th/nvim-cmp",
-  opts = {
-    window = {
-      completion = {
-        winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
-        col_offset = -3,
-        side_padding = 80,
-      },
-      documentation = {
-        side_padding = 80,
-      },
-    },
+  dependencies = {
+    "dcampos/cmp-emmet-vim",
+    "mattn/emmet-vim",
   },
-  -- dependencies = {
-  --   "dcampos/cmp-emmet-vim",
-  -- },
-  -- opts = function(_, opts)
-  --   table.insert(opts.sources, {
-  --     name = "nvim_lsp",
-  --     entry_filter = function(entry, ctx)
-  --       local io = require("io")
-  --       -- only show emmet snippets in lit-like templates
-  --       -- closes the open file
-  --       local file = io.open("/home/flo/log-nvim.log", "a")
-  --       -- appends a word test to the last line of the file
-  --       file:write("-- End of the test.lua file")
-  --       -- if
-  --       --   require("cmp.types").lsp.CompletionItemKind[entry:get_kind()] == "Snippet"
-  --       --   and entry.source:get_debug_name() == "nvim_lsp:emmet_ls"
-  --       -- then
-  --       --   return ctx.in_treesitter_capture("lit_html")
-  --       -- else
-  --       --   return true
-  --       -- end
-  --     end,
-  --   })
-  -- end,
-  -- table.insert(opts.sources, {
-  --   name = "emmet_vim",
-  --   option = {
-  --     filetypes = {
-  --       "css",
-  --       "javascript",
-  --       "typescript",
-  --       "html",
-  --     },
-  --   },
-  -- })
+  opts = function(_, opts)
+    table.insert(opts.sources, {
+      name = "emmet_vim",
+    })
+  end,
 }
